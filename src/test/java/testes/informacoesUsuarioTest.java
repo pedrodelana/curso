a803610592d812f8e03e7298e56284513dc93483
Pedro Lana <pedroalana@hotmail.com>
2019-07-31 20:24:25 -0300

    Inicio do teste

---
+++ src/test/java/testes/informacoesUsuarioTest.java
@@ -0,0 +1,16 @@
+package testes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class informacoesUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+        Assert.assertEquals(1,1);
+
+
+    }
+
+
+
+}
